a8b8827080c7026dd5ac8a35503da9715b6616ec
akshitahlawat07 <akshitahlawat262@gmail.com>
2018-06-28 12:07:14 +0530

    assignment 1

---
+++ akshitahlawat071.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>D3AD GAMING CLAN</title>
+<style type="text/css">
+.header{
+  width:100%;
+  height:200px;
+  background-color:#00CCFF;
+  float:left;
+  text-align:center;
+  color:#FF0000;
+  font:Arial, Helvetica, sans-serif;
+  font-size:100px;
+  }
+.menu
+{
+ width:100%;
+  height:50px;
+  background-color:red;
+  float:left;
+  text-align:center;
+  color:#99FFFF;
+}
+
+.centercontent
+{
+  width:100%;
+  height:300px;
+  background-image:url(1.jpg);
+  float:left;
+  text-align:center;
+  color:#99FFFF;
+}
+
+.leftcontent
+{
+ width:50%;
+  height:250px;
+  background-color:#FF9933;
+  float:left;
+  text-align:center;
+  color:#99FFFF;
+}
+.rightcontent
+{
+ width:50%;
+  height:250px;
+  background-color:#33CC33;
+  float:left;
+  text-align:center;
+  color:#99FFFF;
+}
+.footer{
+  width:100%;
+  height:200px;
+  background-color:#009900;
+  float:left;
+  text-align:center;
+  color:#FF0000;
+  font-size:100px;
+  }
+
+</style>
+</head>
+
+<body>
+<div class="header">
+D3AD GAMING CLAN
+</div>
+
+<div class="menu">
+</div>
+<div class="centercontent">
+</div>
+
+<div class="leftcontent">
+</div>
+
+<div class="rightcontent">
+</div>
+
+<div class="footer">
+Location
+</div>
+</body>
+</html>
